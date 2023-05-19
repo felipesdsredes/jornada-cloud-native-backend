e98f8e4c00f8cd2f476f5b3bba6d454be35c29b5
Guilherme Filippini <guilhermerfilippini@hotmail.com>
2023-05-19 20:28:40 -0300

    Changing mongodb configs to use remote cluster of mongo

--- src/utils/mongodb.ts
+++ src/utils/mongodb.ts
@@ -1,18 +1,32 @@
-import { connect, model, Schema } from "mongoose"
+import { model, Schema } from "mongoose"
 import dotenv from "dotenv"
 dotenv.config()
 
-connect(
-  "mongodb://localhost:27017/jornada-cloud-native",
-  {
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster-jornada-colabor.0ij5x9x.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useFindAndModify: false,
-  },
-  (err) => {
-    err ? console.log(err) : console.log("MongoDB conectado com sucesso")
+    useFindAndModify: false
   }
-)
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 const dataSchema = new Schema(
   {
